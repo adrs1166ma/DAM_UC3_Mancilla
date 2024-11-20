360eefde68f7e2f2bf9b0a7746d68edfd7b47ca5
adrs1166ma <adrsma16@gmail.com>
2024-11-20 17:01:13 -0500

    Finalizado

--- app/src/main/java/com/example/dam_uc3_mancilla/MainActivity.java
+++ app/src/main/java/com/example/dam_uc3_mancilla/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.dam_uc3_mancilla;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Asocia el botón a un OnClickListener
+        Button btnSearchFlights = findViewById(R.id.btnSearchFlights);
+        btnSearchFlights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Muestra un mensaje cuando el botón es presionado
+                Toast.makeText(MainActivity.this, "Se envió su solicitud de vuelo", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
